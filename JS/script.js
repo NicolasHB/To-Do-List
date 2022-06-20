87cb928bccad9476295608c6c4f9ab92acb606e4
NicolasHB <brierenicolas0@gmail.com>
2022-06-20 10:58:30 +0200

    delete done

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
 document.getElementById("push").onclick = function () {
   let elInput = document.getElementById("todo-input").value;
-  
+
   //CONDITIONS
 
   if (elInput == "") {
@@ -11,11 +11,34 @@ document.getElementById("push").onclick = function () {
     alert("Tâche trop longue");
   } else {
     console.log("tout est ok");
+    //Ajoute une tâche
     document.getElementById("tasks").innerHTML += `
-    <div class="tasks d-flex justify-content-between align-items-center mb-3">
-    <span class="my-0" id="taskName">${elInput}</span>
-    <button class="btn btn-danger ">Delete</button>
+    <div class="tasks d-flex justify-content-between align-items-center mb-3 border-bottom py-3">
+    <span class="my-0 tasksDone">${elInput}</span>
+    <button class="btn btn-danger delete ">Delete</button>
     </div>
     `;
+
+    //DELETE TASKS
+    let tasksDelete = document.querySelectorAll(".delete");
+    console.log("tasksDelete", tasksDelete);
+    //on créer une boucle for pour selectionner les élément 1 par 1
+    for (let i = 0; i < tasksDelete.length; i++) {
+      tasksDelete[i].onclick = function () {
+        console.log("click Delete");
+        //séléctionne le noeud parent
+        this.parentNode.remove();
+      };
+    }
+
+    //TASKS DONE
+    let tasksDone = document.querySelectorAll(".tasksDone");
+    for (let i = 0; i < tasksDone.length; i++) {
+      tasksDone[i].onclick = function () {
+        console.log("click Done");
+        this.classList.toggle("completed");
+      };
+    }
+    document.querySelector("#todo-input").value = "";
   }
 };
